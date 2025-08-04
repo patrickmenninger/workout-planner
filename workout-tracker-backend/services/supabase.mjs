af1df86bf7abc4b9e9afaeea4fbb3c64e81a49bf
Patrick Menninger <ptmennin@ncsu.edu>
2025-08-04 15:53:19 -0400

    test

--- workout-tracker-backend/services/supabase.mjs
+++ workout-tracker-backend/services/supabase.mjs
@@ -2,9 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log(process.env);
-console.log(process.env.SUPABASE_URL);
-console.log(process.env.SUPABASE_KEY);
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 export default supabase;
