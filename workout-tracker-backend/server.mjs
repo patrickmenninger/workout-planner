356f0ff78e7e41a9440f09bdd6b67a0c3cafb8f7
Patrick Menninger <ptmennin@ncsu.edu>
2025-08-04 14:49:17 -0400

    test

--- workout-tracker-backend/server.mjs
+++ workout-tracker-backend/server.mjs
@@ -1,12 +1,10 @@
 import express from 'express';
-import cors from 'cors';
 
 import workoutRouter from './routes/workoutRoutes.mjs';
 import exerciseRouter from './routes/exerciseRoutes.mjs';
 import planRouter from './routes/planRoutes.mjs';
 
 const app = express();
-app.use(cors());
 app.use(express.json());
 
 // Add the workout routes
@@ -14,5 +12,4 @@ app.use('/api/workouts', workoutRouter);
 app.use('/api/exercises', exerciseRouter);
 app.use('/api/plans', planRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
